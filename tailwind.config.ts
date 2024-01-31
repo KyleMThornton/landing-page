a35cdd2648c6d1f0d75cb272934873139663e450
KyleMThornton <kylemthornton@gmail.com>
2024-01-31 13:21:45 -0800

    Add custom slowpulse animation and mega text size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,23 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      animation: {
+        slowpulse: 'slowpulse 40s infinite',
+      },
+      keyframes: {
+        slowpulse: {
+          '0%': { transform: 'scale(1)' },
+          '50%': { transform: 'scale(1.5)' },
+          '100%': { transform: 'scale(1)' },
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontSize: {
+        'mega': '20rem',
       },
     },
   },
